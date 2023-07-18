ab00af662d61167e7b2a6da7f3ea174f712d8f5e
aman-17 <amanrangapur@gmail.com>
2023-07-17 23:21:17 -0500

    eval update

--- evaluation.py
+++ evaluation.py
@@ -3,11 +3,19 @@ import json
 with open("finfact.json", "r") as infile:
     data = json.load(infile)
 
-titles = [entry["title"] for entry in data]
-sentences = [item["sentence"] for entry in data for item in entry["data"]]
-labels = [entry["label"] for entry in data]
+# titles = [entry["title"] for entry in data]
+# sentences = [item["sentence"] for entry in data for item in entry["data"]]
+# labels = [entry["label"] for entry in data]
 
-print("Title:", titles)
-print("Sentences:", sentences)
-print("Labels:", labels)
+# print("Title:", titles)
+# print("Sentences:", sentences)
+# print("Labels:", labels)
 
+for entry in data:
+    title = entry["title"]
+    sentences = entry["data"]
+    sentence_lengths = [len(sentence["sentence"]) for sentence in sentences]
+    
+    print("Title:", title)
+    print("Sentence Lengths:", sentences)
+    print("---")
